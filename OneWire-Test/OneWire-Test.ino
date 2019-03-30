ea23a9eadd87e389dc79f5b6c9938bdbc4d27638
Stephen Catsamas <seca8193@gmail.com>
2019-03-30 19:35:14 +1100

    Added cable sensor ordering file

--- OneWire-Test/OneWire-Test.ino
+++ OneWire-Test/OneWire-Test.ino
@@ -16,7 +16,7 @@ DallasTemperature sensors(&oneWire);
 void setup(void) 
 { 
  // start serial port 
- Serial.begin(9600); 
+ Serial.begin(57600); 
  Serial.println("Dallas Temperature IC Control Library Demo"); 
  // Start up the library 
  sensors.begin(); 
@@ -30,14 +30,29 @@ void loop(void)
  sensors.requestTemperatures(); // Send the command to get temperature readings 
  //Serial.println("DONE"); 
 /********************************************************************/
- Serial.print("Temp 0: "); 
+ 
+ for (int i=0; i<10; i++){
+     Serial.print(" Temp ");
+     Serial.print(i);
+     Serial.print(": ");
+     Serial.print(sensors.getTempCByIndex(i));
+ }
+ Serial.println();
+ 
+ /* Serial.print("Temp 0: "); 
  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? 
  Serial.print(" Temp 1: "); 
  Serial.print(sensors.getTempCByIndex(1));
  Serial.print(" Temp 2: "); 
  Serial.print(sensors.getTempCByIndex(2));
+  Serial.print(" Temp 2: "); 
+  Serial.print(sensors.getTempCByIndex(2));
+  Serial.print(" Temp 2: "); 
+ Serial.print(sensors.getTempCByIndex(2));
+  Serial.print(" Temp 2: "); 
+ Serial.print(sensors.getTempCByIndex(2));
  Serial.print(" Temp 3: ");
- Serial.println(sensors.getTempCByIndex(3)); // Why "byIndex"?  
+Serial.println(sensors.getTempCByIndex(3)); //Why "byIndex"?  */
    // You can have more than one DS18B20 on the same bus.  
    // 0 refers to the first IC on the wire 
    delay(1000); 
