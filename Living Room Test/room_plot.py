7a8ebbdcb324e80d5a6975bc09a136d0ce4da6ed
Boris Deletic <boris.deletic@monash.edu>
2019-04-07 16:57:35 +1000

    added color animation to 2d plot

--- Living Room Test/room_plot.py
+++ Living Room Test/room_plot.py
@@ -2,11 +2,13 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from matplotlib.colors import ListedColormap
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 import data_analysis
 import pandas as pd
 import re
+import matplotlib.animation as animation
 
 
 class LivingRoom():
@@ -14,7 +16,7 @@ class LivingRoom():
         data = data_analysis.DataAnalysis("H_data.csv")
         self.df = data.df
         self.layout_room("room_layout.csv", 'sensor_layout.csv')
-        self.plot()
+        self.plot2D()
 
     def layout_room(self, room_file, sensor_file):
         with open(room_file) as f:
@@ -35,12 +37,7 @@ class LivingRoom():
 
         self.sen_pos = np.genfromtxt(sensor_file, delimiter = ',').T
         
-##        print(verts.T)
-##        fig, ax = plt.subplots()
-##        ax.plot(verts[0], verts[1])
-##        ax.scatter(sen_data[0], sen_data[1], color='red')
-##        plt.axis('scaled')
-##        plt.show()
+
 
     def add_verticies(self, verts, dist, bearings):
         for i in range(len(bearings)):
@@ -67,16 +64,19 @@ class LivingRoom():
         boundsX = [self.verts[0].min(), self.verts[0].max()]
         boundsY = [self.verts[1].min(), self.verts[1].max()]
 
-        res = 50
+        res = 1
+        padding = 50
         
-        X = np.arange(boundsX[0] - res, boundsX[1] + res, res)
-        Y = np.arange(boundsY[0] - res, boundsY[1] + res, res)
-        
-        
+        X = np.arange(boundsX[0] - padding, boundsX[1] + padding, res)
+        Y = np.arange(boundsY[0] - padding, boundsY[1] + padding, res)
+        X, Y = np.meshgrid(X, Y)
 
-    def plot(self):
-        print(self.df.loc[0][3])
-        
+        print(X[2], Y[2])
+
+
+    def plot3D(self):
+        self.format_3D_data()
+
         fig = plt.figure()
         ax = fig.gca(projection='3d')
 
@@ -84,7 +84,6 @@ class LivingRoom():
         X = np.arange(-5, 5, 0.25)
         Y = np.arange(-5, 5, 0.25)
 
-        print(X,Y)
         X, Y = np.meshgrid(X, Y)
         Z = np.zeros(X.shape)
         Z[6][5] = 1
@@ -103,6 +102,38 @@ class LivingRoom():
         fig.colorbar(surf, shrink=0.5, aspect=5)
 
         plt.show()
+
+
+    def plot2D(self):
+        self.temps = np.vstack(self.df.values[:, 1:]).astype(np.float)
+
+
+        print(self.temps)
+        fig, ax = plt.subplots()
+
+        r = np.transpose([np.linspace(1, 0.6,10)])
+        g = np.transpose([np.zeros(10)])
+        b = np.transpose([np.zeros(10)])
+        colors = np.concatenate((r,g,b), axis=1)
+        colormap = ListedColormap(colors)
+
+        norm = plt.Normalize(self.temps.min(), self.temps.max())
+        self.sc = ax.scatter(self.sen_pos[0], self.sen_pos[1], c=self.temps[0], s=500, cmap=cm.coolwarm, norm = norm)
+
+     #   ax.scatter(self.sen_pos[0], self.sen_pos[1], c=temps, cmap = colormap, s = 400)
+        ax.plot(self.verts[0], self.verts[1], color = 'black')        
+        
+        ax.set_facecolor((0.8,0.8,0.8))
+        plt.axis('scaled')
+
+        dt = 50
+        ani = animation.FuncAnimation(fig, self.animate, frames=range(len(self.temps)), interval = dt)
+        plt.show()
+
+    def animate(self, i):
+        self.sc.set_array(self.temps[i])
+        
+        
 
 
 LivingRoom()
